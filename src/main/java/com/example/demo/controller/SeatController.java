bf59fbb3085b66cf52a8ee850c43d3f9c19cb73b
khaiminh0401 <85891822+khaiminh0401@users.noreply.github.com>
2023-09-27 22:28:46 +0700

    delete code

--- src/main/java/com/example/demo/controller/SeatController.java
+++ src/main/java/com/example/demo/controller/SeatController.java
@@ -1,9 +1,5 @@
 package com.example.demo.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.service.MovieService;
 import com.example.demo.service.SeatService;
 
 @RestController
@@ -22,9 +17,6 @@ public class SeatController {
     @Autowired
     private SeatService seatService;
 
-    @Autowired
-    private MovieService movieService;
-
     @GetMapping(value ={"/",""})
     public ResponseEntity<?> findByRoomId(@RequestParam("roomid") String roomId){
         return ResponseEntity.ok(seatService.findByRoomId(roomId));
@@ -32,9 +24,6 @@ public class SeatController {
 
     @GetMapping("/getSeatHasCheckTicket")
     public ResponseEntity<?> getSeatHasCheckTicket(@RequestParam("id") int id){
-        // Map<String,Object> map = new HashMap<>();
-        // map.put("seat",seatService.getSeatHasCheckTicket(id));
-        // map.put("movie",movieService.findByShowTimeId(id));
         return ResponseEntity.ok(seatService.getSeatHasCheckTicket(id));
     }
 
