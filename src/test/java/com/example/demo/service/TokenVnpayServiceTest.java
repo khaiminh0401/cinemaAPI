c466ad6a5a681ea087efd312956d199eb36073ef
Bùi Vũ Thiên Ân (anbvt) <anbvt562003@gmail.com>
2023-11-10 07:02:01 +0700

    Modify to match the master branch

--- src/test/java/com/example/demo/service/TokenVnpayServiceTest.java
+++ src/test/java/com/example/demo/service/TokenVnpayServiceTest.java
@@ -1,6 +1,9 @@
 package com.example.demo.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +11,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.demo.MovieTestApplication;
+import com.example.demo.admin.controller.enums.RequestParameterEnum;
 import com.example.demo.config.GsonService;
 import com.example.demo.entity.TokenVnpay;
+import com.example.demo.exception.InvalidRequestParameterException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest(classes = MovieTestApplication.class)
 @AutoConfigureMockMvc
@@ -20,12 +26,47 @@ public class TokenVnpayServiceTest {
     @Autowired
     TokenVnpayService tokenVnpayService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     public void insert() throws Exception {
         TokenVnpay tokenVnpay = GsonService.toObject(gsonService.getValueInput(this.getClass().toString(), "insert"),
                 TokenVnpay.class);
-        Integer result = tokenVnpayService.insert(tokenVnpay);
-        Integer expect = 1;
+        String result = tokenVnpayService.insert(Optional.of(tokenVnpay));
+        String expect = "SUCCESS";
         assertEquals(result, expect);
     }
+
+    @Test
+    public void insertIsNull() throws Exception {
+        Throwable exception = assertThrows(InvalidRequestParameterException.class, () -> {
+            throw new InvalidRequestParameterException("TokenVNPay", RequestParameterEnum.NOTHING);
+        });
+        try {
+            tokenVnpayService.insert(Optional.of(null));
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), exception.getMessage());
+        }
+    }
+
+    @Test
+    public void findByCustomerId() throws Exception {
+        String expect = gsonService.getValueExpect(this.getClass().toString(), "findByCustomerId");
+        String result = objectMapper.writeValueAsString(tokenVnpayService.findByCustomerId(Optional.of(1)));
+        assertEquals(expect, result);
+    }
+
+    @Test
+    public void findByCustomerIdIsNull() throws Exception {
+        Throwable exception = assertThrows(InvalidRequestParameterException.class, () -> {
+            throw new InvalidRequestParameterException("CustomerId", RequestParameterEnum.NOTHING);
+        });
+        try {
+            tokenVnpayService.findByCustomerId(Optional.of(null));
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), exception.getMessage());
+        }
+    }
+
 }
