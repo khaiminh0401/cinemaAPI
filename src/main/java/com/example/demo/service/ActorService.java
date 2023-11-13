26e70421c0f85bd33eaac8257792efd06c61f062
dkminh.hcm <dkminh.hcm@vnpt.vn>
2023-11-14 00:28:06 +0700

    add log

--- src/main/java/com/example/demo/service/ActorService.java
+++ src/main/java/com/example/demo/service/ActorService.java
@@ -1,7 +1,8 @@
 package com.example.demo.service;
 
 import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +16,18 @@ public class ActorService {
 	@Autowired
 	private ActorDao actorDao;
 
+	private static final Logger logger = LoggerFactory.getLogger(ActorService.class);
+
 	public List<Actor> findAll() {
 		return actorDao.findAll();
 	}
 
 	public RequestStatusEnum insert(Actor actor) throws InvalidRequestParameterException {
-		return actorDao.insert(actor) == 1 ? RequestStatusEnum.SUCCESS : RequestStatusEnum.FAILURE;
+		logger.info("RUN -> ActorService");
+		logger.info("params: "+actor.toString());
+		RequestStatusEnum response = actorDao.insert(actor) == 1 ? RequestStatusEnum.SUCCESS : RequestStatusEnum.FAILURE;
+		logger.info("Status:"+response);
+		return response;
 	}
 
 }
