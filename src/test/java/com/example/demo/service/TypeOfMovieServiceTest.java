0e8b043f76607704d3d453fc0ad3b235241b89c6
Bùi Vũ Thiên Ân (anbvt) <anbvt562003@gmail.com>
2023-11-18 12:26:40 +0700

    Modify test typeOfMovieService

--- src/test/java/com/example/demo/service/TypeOfMovieServiceTest.java
+++ src/test/java/com/example/demo/service/TypeOfMovieServiceTest.java
@@ -7,13 +7,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 import com.example.demo.MovieTestApplication;
 import com.example.demo.config.GsonService;
 import com.example.demo.exception.InvalidRequestParameterException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
 
 @SpringBootTest(classes = MovieTestApplication.class)
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
 @AutoConfigureMockMvc
 public class TypeOfMovieServiceTest {
     @Autowired
